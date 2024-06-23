dfec662d73c50227b9f2e989e53179e5596b2443
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2024-06-24 02:10:00 +0800

    Code-part2

--- resources/views/user/tfa-settings.blade.php
+++ resources/views/user/tfa-settings.blade.php
@@ -1,29 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h2>Welcome {{ auth()->user()->name }}</h2>
-        <form method="POST" action="{{ url('/user/two-factor-authentication') }}">
-            @csrf
+<div class="container">
+    <div class="row justify-content-center text-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Two Factor Authentication') }}</div>
+                <div class="card-body">
+                    @if (session('status') == "two-factor-authentication-disabled")
+                        <div class="alert alert-danger" role="alert">
+                            Two factor authentication has been disabled
+                        </div>
+                    @endif
 
-            @if(auth()->user()->two_factor_secret)
-                Two factor authentication is enabled.
-                <h3>QR Code for authenticator applications</h3>
-                <div class="pt-5 pb-5">
-                    {!!  auth()->user()->twoFactorQrCodeSvg() !!}
+                    @if (session('status') == "two-factor-authentication-enabled")
+                        <div class="alert alert-success" role="alert">
+                            Two factor authentication has been enabled
+                        </div>
+                    @endif
+                    <form method="post" action="/user/two-factor-authentication">
+                        @csrf
+                        @if (auth()->user()->two_factor_secret)
+                            @method('DELETE')
+                            <div class="pb-3">
+                                {!! auth()->user()->twoFactorQrCodeSvg() !!}
+                            </div>
+                            <div class="mt-4">
+                                <h3>Recovery Codes</h3>
+                                <ul class="list-group mb-2">
+                                    @foreach (json_decode(decrypt(auth()->user()->two_factor_recovery_codes)) as $code)
+                                        <li class="list-group-item">{{ $code }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <button class="btn btn-danger">
+                                Disable
+                            </button>
+                        @else
+                            <button class="btn btn-success">
+                                Enable
+                            </button>
+                        @endif
+                    </form>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button class="btn btn-danger" type="submit">Logout</button>
+                    </form>
                 </div>
-                <h3>Recovery codes</h3>
-                <ul>
-                    @foreach(auth()->user()->recoveryCodes() as $code)
-                        <li>{{ $code }}</li>
-                    @endforeach
-                </ul>
-                @method('DELETE')
-                <button class="btn btn-danger">Disable</button>
-            @else
-                Two factor authentication is not enabled.
-                <button class="btn btn-primary">Enable</button>
-            @endif
-        </form>
+            </div>
+        </div>
     </div>
+</div>
+
 @endsection
